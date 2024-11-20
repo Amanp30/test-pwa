14361bae39037dd390e9f68e56c8c6bb61ef57b6
Aman Pareek <107720919+Amanp30@users.noreply.github.com>
2024-11-20 14:56:15 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(function sayHello(){return"hello, ".concat(arguments.length>0&&void 0!==arguments[0]?arguments[0]:"aman")}("tushar"))})();
---
+++ serviceworker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "pwa-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/index.html",
+  "/assets/css/main.css",
+  "/about.html",
+  "/assets/js/main.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: Caching Assets");
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log("Service Worker: Deleting old cache", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        console.log(
+          "Service Worker: Returning cached response for",
+          event.request.url
+        );
+        return cachedResponse;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+          console.log(
+            "Service Worker: Caching new resource",
+            event.request.url
+          );
+        });
+        return response;
+      });
+    })
+  );
+});
